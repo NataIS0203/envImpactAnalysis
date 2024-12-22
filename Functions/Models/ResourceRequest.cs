4ee40a30462f18939f8b5821381077f7b6b1c32e
Natalya Sniff <natalyasniff@gmail.com>
2024-12-21 23:55:11 -0600

    Adding new falues

---
+++ Functions/Models/ResourceRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Durable.Functions.Models
+{
+    class ResourceRequest : BaseRequest
+    {
+       public required string Resource { get; set; }
+    }
+}
---
+++ Service/Models/BaseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Durable.Service.Models
+{
+    class BaseModel
+    {
+       public string? Region { get; set; }
+
+       public int? Percentage { get; set; }
+    }
+}
